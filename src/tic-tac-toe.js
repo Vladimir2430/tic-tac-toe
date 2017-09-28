eab7ee07a07c58873243e04be166fb220969a122
Vladimir <31570004+Vladimir2430@users.noreply.github.com>
2017-09-29 01:37:28 +0300

    Update tic-tac-toe.js

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,47 +1,48 @@
 class TicTacToe {
     constructor() {
-        this.aaa='x';
-        this.bbb=[
+        this.curPl='x';
+        this.field=[
             [null,null,null],
             [null,null,null],
             [null,null,null]
         ]
-        this.ccc=null;
-        this.ddd=false;
+        this.winner=null;
+        this.isWin=false;
     }
 
     getCurrentPlayerSymbol() {
-        return this.aaa;
+        return this.curPl;
     }
 
     nextTurn(rowIndex, columnIndex) {
-         if(!this.bbb[rowIndex][columnIndex]){
-            this.field[rowIndex][columnIndex]=this.aaa;
-            (this.aaa==='x')?this.aaa='o':this.aaa='x';
+        if(!this.field[rowIndex][columnIndex]){
+            this.field[rowIndex][columnIndex]=this.curPl;
+            (this.curPl==='x')?this.curPl='o':this.curPl='x';
         }
     }
 
     isFinished() {
-        this.ddd=((this.bbb[0][0]==this.bbb[0][1])&&(this.bbb[0][1]==this.bbb[0][2])&&this.bbb[0][2]!=null)||
-            ((this.bbb[1][0]==this.bbb[1][1])&&(this.bbb[1][1]==this.bbb[1][2])&&this.bbb[1][2]!=null)||
-            ((this.bbb[2][0]==this.bbb[2][1])&&(this.bbb[2][1]==this.bbb[2][2])&&this.bbb[2][2]!=null)||
-            ((this.bbb[0][0]==this.bbb[1][0])&&(this.bbb[1][0]==this.bbb[2][0])&&this.bbb[2][0]!=null)||
-            ((this.bbb[0][1]==this.bbb[1][1])&&(this.bbb[1][1]==this.bbb[2][1])&&this.bbb[2][1]!=null)||
-            ((this.bbb[0][2]==this.bbb[1][2])&&(this.bbb[1][2]==this.bbb[2][2])&&this.bbb[2][2]!=null)||
-            ((this.bbb[0][0]==this.bbb[1][1])&&(this.bbb[1][1]==this.bbb[2][2])&&this.bbb[2][2]!=null)||
-            ((this.bbb[0][2]==this.bbb[1][1])&&(this.bbb[1][1]==this.bbb[2][0])&&this.bbb[2][0]!=null);
-        return this.ddd||this.noMoreTurns();
+        this.isWin=((this.field[0][0]==this.field[0][1])&&(this.field[0][1]==this.field[0][2])&&this.field[0][2]!=null)||
+                    ((this.field[1][0]==this.field[1][1])&&(this.field[1][1]==this.field[1][2])&&this.field[1][2]!=null)||
+                    ((this.field[2][0]==this.field[2][1])&&(this.field[2][1]==this.field[2][2])&&this.field[2][2]!=null)||
+                    ((this.field[0][0]==this.field[1][0])&&(this.field[1][0]==this.field[2][0])&&this.field[2][0]!=null)||
+                    ((this.field[0][1]==this.field[1][1])&&(this.field[1][1]==this.field[2][1])&&this.field[2][1]!=null)||
+                    ((this.field[0][2]==this.field[1][2])&&(this.field[1][2]==this.field[2][2])&&this.field[2][2]!=null)||
+                    ((this.field[0][0]==this.field[1][1])&&(this.field[1][1]==this.field[2][2])&&this.field[2][2]!=null)||
+                    ((this.field[0][2]==this.field[1][1])&&(this.field[1][1]==this.field[2][0])&&this.field[2][0]!=null);
+        return this.isWin||this.noMoreTurns();
+
     }
 
-    getWinner() {
-         if(this.isFinished()&&this.ddd)
-            return this.aaa==='x'?'o':'x';
-         return null;
+    getWinner() {//does not work
+        if(this.isFinished()&&this.isWin)
+            return this.curPl==='x'?'o':'x';
+        return null;
     }
 
     noMoreTurns() {
-        for(let nn of this.bbb){
-            if(nn.includes(null)) return false; 
+        for(let el of this.field){
+            if(el.includes(null)) return false; 
         }
         return true;
     }
@@ -51,7 +52,7 @@ class TicTacToe {
     }
 
     getFieldValue(rowIndex, colIndex) {
-         return this.bbb[rowIndex][colIndex];
+        return this.field[rowIndex][colIndex];
     }
 }
 
